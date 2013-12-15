4af73b00812964b427ca00104453f1e869fbc132
sleeping-lion <toughjjh@gmail.com>
2013-12-16 01:28:04 +0900

    gg

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,12 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
+set :application, 'plan'
+set :repo_url, 'git@github.com:sleeping-lion/plan.git'
+set :branch, 'master'
+set :deploy_to, '/home/deploy/plan'
 # set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
-# set :pty, true
+set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
